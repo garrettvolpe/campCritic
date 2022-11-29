b6a72e0a4be69a5476ee1cd0e4bcaeef12b17034
Garrett Volpe <garrettvolpe@gmail.com>
2022-11-29 12:47:38 -0500

    add images to campgrounds

--- seeds/index.js
+++ seeds/index.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const Campground = require('../models/campground');
 const cities = require('./cities')
+const axios = require('axios')
 const { places, descriptors } = require('./seedHelpers')
 
 mongoose.connect('mongodb+srv://garrett:WbPC3uL0rNRm0ltC@campgrounds.pcpwmwg.mongodb.net/campgrounds?retryWrites=true&w=majority', {
@@ -18,17 +19,41 @@ db.once("open", () => {
 
 const sample = (array) => array[Math.floor(Math.random() * array.length)];
 
+
+// call unsplash and return small image
+async function seedImg() {
+    try {
+      const resp = await axios.get('https://placeimg.com/640/480/nature')
+    }
+    catch{
+        console.log("failed pic")
+    }
+}
+
 const seedDB = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i < 50; i++) {
-        const random1000 = Math.floor(Math.random() * 1000);
+        const placeSeed = Math.floor(Math.random() * places.length)
+        const descriptorsSeed = Math.floor(Math.random() * descriptors.length)
+        const citySeed = Math.floor(Math.random() * cities.length)
+        const price = Math.floor(Math.random() * 20) + 10;
+    
+     
+        // seed data into campground
         const camp = new Campground({
-            location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+          imageUrl: await seedImg(),
+          title: `${descriptors[descriptorsSeed]} ${places[placeSeed]}`,
+          image: 'https://placeimg.com/640/480/nature',
+          location: `${cities[citySeed].city}, ${cities[citySeed].state}`,
+          description:
+            'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Debitis, nihil tempora vel aspernatur quod aliquam illum! Iste impedit odio esse neque veniam molestiae eligendi commodi minus, beatae accusantium, doloribus quo!',
+            price: price
         })
-        await camp.save();
+
+     
+        await camp.save()
+      }
     }
-}
 
 seedDB().then(() => {
     mongoose.connection.close();
