2cb9adff5eb8954c50cfef5a4dcfe0f16e8a6fe1
Garrett Volpe <garrettvolpe@gmail.com>
2022-12-13 12:13:56 -0500

    add styling and delete function for reviews

--- app.js
+++ app.js
@@ -76,7 +76,6 @@ app.post('/campgrounds', validateCampground, catchAsync(async (req, res, next) =
 
 app.get('/campgrounds/:id', catchAsync(async (req, res, next) => {
     const campground = await Campground.findById(req.params.id).populate('reviews')
-    console.log(campground)
     res.render('campgrounds/show', { campground })
 }))
 
@@ -107,6 +106,13 @@ app.post('/campgrounds/:id/reviews', validateReview, catchAsync(async (req, res,
     res.redirect(`/campgrounds/${campground._id}`);
 
 }));
+
+app.delete('/campgrounds/:id/reviews/:reviewId', catchAsync(async(req,res) =>{
+    const {id, reviewId} = req.params;
+    Campground.findByIdAndUpdate(id, {$pull: {reviews:reviewId}});
+    await Review.findByIdAndDelete(reviewId);
+    res.redirect(`/campgrounds/${id}`);
+}))
 
 app.all('*', (req, res, next) => {
     next(new ExpressError("page not found!", 404))
