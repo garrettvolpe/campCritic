01a489f3844633917b32d652e5133b95599a0e22
Garrett Volpe <garrettvolpe@gmail.com>
2022-12-16 10:26:17 -0500

    set up session

--- app.js
+++ app.js
@@ -3,13 +3,12 @@ const path = require('path');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
 const methodOverride = require('method-override');
-
 const ExpressError = require('./utils/ExpressError')
 const Joi = require('joi');
 const { campgroundSchema, reviewSchema } = require('./schemas.js')
 const { join } = require('path');
 const review = require('./models/review');
-
+const session = require('express-session');
 
 const campgrounds = require('./routes/campgrounds');
 const reviews = require('./routes/reviews');
@@ -34,10 +33,18 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
-
 app.use(express.static(path.join(__dirname,'public')));
-
-
+const sessionConfig = {
+    secret: 'thisismysecret',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        httpOnly: true, 
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+        maxAge: 1000 * 60 * 60 * 24 * 7
+    }
+}
+app.use(session(sessionConfig));
 
 
 
