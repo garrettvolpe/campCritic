9dc145b2c2c851c468217106add1260ee4f687d8
Garrett Volpe <garrettvolpe@gmail.com>
2022-12-16 10:17:58 -0500

    add static asset

--- app.js
+++ app.js
@@ -35,6 +35,8 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 
+app.use(express.static(path.join(__dirname,'public')));
+
 
 
 
