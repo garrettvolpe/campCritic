34924d3e04a3865b726f3f2c9776b5b91e9ec3e1
Garrett Volpe <109316247+garrettvolpe@users.noreply.github.com>
2022-12-19 17:23:19 -0500

    add return behavior for login routes

--- app.js
+++ app.js
@@ -39,13 +39,13 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
-app.use(express.static(path.join(__dirname,'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 const sessionConfig = {
     secret: 'thisismysecret',
     resave: false,
     saveUninitialized: true,
     cookie: {
-        httpOnly: true, 
+        httpOnly: true,
         expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
         maxAge: 1000 * 60 * 60 * 24 * 7
     }
@@ -61,14 +61,15 @@ passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 
-app.use((req,res,next)=>{
+app.use((req, res, next) => {
+    res.locals.currentUser = req.user;
     res.locals.success = req.flash("success");
     res.locals.error = req.flash("error");
     next();
 });
 
-app.get('/fakeUser', async(req,res)=>{
-    const user = new User({email: "gv@gmail.com", username: "gvolpe2592"});
+app.get('/fakeUser', async (req, res) => {
+    const user = new User({ email: "gv@gmail.com", username: "gvolpe2592" });
     const newUser = await User.register(user, 'monkey')
     res.send(newUser)
 })
--- middleware.js
+++ middleware.js
@@ -1,5 +1,8 @@
-module.exports.isLoggedIn = (req,res,next)=>{
-    if(!req.isAuthenticated()){
+module.exports.isLoggedIn = (req, res, next) => {
+    req.session.returnTo = req.originalUrl
+    console.log("REQ.USER>>>", req.user)
+    if (!req.isAuthenticated()) {
+        req.session.returnTo = req.originalUrl
         req.flash("error", "You must be signed in!")
         return res.redirect('/login')
     }
