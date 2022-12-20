9a134119830764b004855d44237e3c5547c3152c
Garrett Volpe <109316247+garrettvolpe@users.noreply.github.com>
2022-12-19 21:02:00 -0500

    add review and user controllers

--- routes/users.js
+++ routes/users.js
@@ -3,47 +3,17 @@ const { model } = require('mongoose');
 const { register } = require('../models/user');
 const router = express.Router();
 const catchAsync = require('../utils/catchAsync')
-const User = require('../models/user');
+const users = require('../controllers/users');
 const passport = require('passport');
 
-router.get('/register', (req, res) => {
-    res.render('users/register')
-})
+router.get('/register', users.renderRegister);
 
-router.post('/register', catchAsync(async (req, res) => {
-    try {
-        const { email, username, password } = req.body;
-        const user = new User({ email, username });
-        const registeredUser = await User.register(user, password)
-        req.login(registeredUser, err => {
-            if (err) return next(err);
-            req.flash("success", "Welcome to Yelp Camp!");
-            res.redirect('/campgrounds')
-        })
-    } catch (e) {
-        req.flash('error', e.message);
-        res.redirect('register')
-    }
+router.post('/register', catchAsync(users.registerUser));
 
-}));
+router.get('/login', users.renderLogin);
 
-router.get('/login', (req, res) => {
-    res.render('users/login')
-});
+router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login', keepSessionInfo: true }), users.loginUser);
 
-router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login', keepSessionInfo: true }), (req, res) => {
-    req.flash("success", "Welcome back!")
-    const redirectUrl = req.session.returnTo || "/campgrounds"
-    delete req.session.returnTo;
-    res.redirect(redirectUrl)
-});
-
-router.get('/logout', (req, res) => {
-    req.logout(function (err) {
-        if (err) { return next(err); }
-        req.flash("success", "Goodbye!")
-        res.redirect('/campgrounds')
-    })
-})
+router.get('/logout', users.logoutUser)
 
 module.exports = router
