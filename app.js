0da6cdd00dc00f3dfa7648fe7873f511490fda08
Garrett Volpe <109316247+garrettvolpe@users.noreply.github.com>
2022-12-25 10:27:05 -0500

    hiding errors and changing cookies

--- app.js
+++ app.js
@@ -49,11 +49,13 @@ app.use(mongoSanatize());
 
 
 const sessionConfig = {
+    name: 'sesID',
     secret: 'thisismysecret',
     resave: false,
     saveUninitialized: true,
     cookie: {
         httpOnly: true,
+        // secure: true,
         expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
         maxAge: 1000 * 60 * 60 * 24 * 7
     }
