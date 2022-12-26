bc0724e562d6260a868b0953120cf9c60c1febb5
Garrett Volpe <109316247+garrettvolpe@users.noreply.github.com>
2022-12-26 18:45:08 -0500

    updated seeds

--- seeds/index.js
+++ seeds/index.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Campground = require('../models/campground');
 const cities = require('./cities')
 const axios = require('axios')
-const { places, descriptors } = require('./seedHelpers')
+const { places, descriptors, imagesForUpload } = require('./seedHelpers')
 
 mongoose.connect('mongodb+srv://garrett:WbPC3uL0rNRm0ltC@campgrounds.pcpwmwg.mongodb.net/campgrounds?retryWrites=true&w=majority', {
     useNewUrlParser: true,
@@ -34,6 +34,7 @@ const seedDB = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i <100; i++) {
         const placeSeed = Math.floor(Math.random() * places.length)
+        const imageUploadSeed1 = Math.floor(Math.random() * imagesForUpload.length);
         const descriptorsSeed = Math.floor(Math.random() * descriptors.length)
         const citySeed = Math.floor(Math.random() * cities.length)
         const price = Math.floor(Math.random() * 20) + 10;
@@ -52,16 +53,7 @@ const seedDB = async () => {
             description:
                 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Debitis, nihil tempora vel aspernatur quod aliquam illum! Iste impedit odio esse neque veniam molestiae eligendi commodi minus, beatae accusantium, doloribus quo!',
             price: price,
-            images: [
-                {
-                    url: 'https://res.cloudinary.com/djjaxwfvu/image/upload/v1671645294/CampCritic/odj6jb1vusazqbmrqic9.webp',
-                    filename: 'CampCritic/odj6jb1vusazqbmrqic9'
-                  },
-                  {
-                    url: 'https://res.cloudinary.com/djjaxwfvu/image/upload/v1671645294/CampCritic/ooonnoj7sz68o6g1gl1y.webp',
-                    filename: 'CampCritic/ooonnoj7sz68o6g1gl1y'
-                  }
-            ]
+            images: imagesForUpload[imageUploadSeed1]
         });
 
         await camp.save()
--- seeds/seedHelpers.js
+++ seeds/seedHelpers.js
@@ -42,3 +42,67 @@ module.exports.places = [
     'Cliffs',
     'Hollow'
 ]
+
+
+module.exports.imagesForUpload = [
+    {
+        url: 'https://res.cloudinary.com/djjaxwfvu/image/upload/v1671645294/CampCritic/odj6jb1vusazqbmrqic9.webp',
+        filename: 'CampCritic/odj6jb1vusazqbmrqic9'
+      },
+      {
+        url: 'https://res.cloudinary.com/djjaxwfvu/image/upload/v1671645294/CampCritic/ooonnoj7sz68o6g1gl1y.webp',
+        filename: 'CampCritic/ooonnoj7sz68o6g1gl1y'
+      },
+      {
+        url: 'https://res.cloudinary.com/djjaxwfvu/image/upload/v1672097228/CampCritic/camp1.jpg',
+        filename: 'v1672097228/CampCritic/camp1.jpg'
+      },
+      {
+        url: 'https://res.cloudinary.com/djjaxwfvu/image/upload/v1672097228/CampCritic/TPW_TOF_003_800p_ka0plh.jpg',
+        filename: 'v1672097228/CampCritic/TPW_TOF_003_800p_ka0plh.jpg'
+      },
+      {
+        url: 'https://res.cloudinary.com/djjaxwfvu/image/upload/v1672097228/CampCritic/image_ew3snh.jpg',
+        filename: 'v1672097228/CampCritic/image_ew3snh.jpg'
+      },
+      {
+        url: 'https://res.cloudinary.com/djjaxwfvu/image/upload/v1672025930/CampCritic/znkzf4jqw7l9o5tltsxv.jpg',
+        filename: 'v1672025930/CampCritic/znkzf4jqw7l9o5tltsxv.jpg'
+      },
+      {
+        url: 'https://res.cloudinary.com/djjaxwfvu/image/upload/v1671812383/CampCritic/egcdylsmf037x22vghxn.webp',
+        filename: 'v1671812383/CampCritic/egcdylsmf037x22vghxn.webp'
+      },
+      {
+        url: 'https://res.cloudinary.com/djjaxwfvu/image/upload/v1671723883/CampCritic/yysjwaytjjrxv79ezhzn.webp',
+        filename: 'v1671723883/CampCritic/yysjwaytjjrxv79ezhzn.webp'
+      },
+      {
+        url: 'https://res.cloudinary.com/djjaxwfvu/image/upload/v1672098045/CampCritic/images_yzehov.jpg',
+        filename: 'v1672098045/CampCritic/images_yzehov.jpg'
+      },
+      {
+        url: 'https://res.cloudinary.com/djjaxwfvu/image/upload/v1672098045/CampCritic/Camping-at-the-camground-900x525_jpjzze.jpg',
+        filename: 'v1672098045/CampCritic/Camping-at-the-camground-900x525_jpjzze.jpg'
+      },
+      {
+        url: 'https://res.cloudinary.com/djjaxwfvu/image/upload/v1672098045/CampCritic/CAMPING0915-Glacier-National-Park-eb04b1b037d241159003d931c8a46dbc_a6xvau.jpg',
+        filename: 'v1672098045/CampCritic/CAMPING0915-Glacier-National-Park-eb04b1b037d241159003d931c8a46dbc_a6xvau.jpg'
+      },
+      {
+        url: 'https://res.cloudinary.com/djjaxwfvu/image/upload/v1672098045/CampCritic/Colorado_Blue-Lakes-Telluride_Sprinter-Van-Dispersed-Camping-Drone-1_cdwm0v.jpg',
+        filename: 'v1672098045/CampCritic/Colorado_Blue-Lakes-Telluride_Sprinter-Van-Dispersed-Camping-Drone-1_cdwm0v.jpg'
+      },
+      {
+        url: 'https://res.cloudinary.com/djjaxwfvu/image/upload/v1672098045/CampCritic/cedar_bloom_oregon_hipcamp_of5c1r_h4jdkk.jpg',
+        filename: 'v1672098045/CampCritic/cedar_bloom_oregon_hipcamp_of5c1r_h4jdkk.jpg'
+      },
+      {
+        url: 'https://res.cloudinary.com/djjaxwfvu/image/upload/v1672098045/CampCritic/no-reservation-Twenty20_c95cdl.webp',
+        filename: 'v1672098045/CampCritic/no-reservation-Twenty20_c95cdl.webp'
+      },
+      {
+        url: 'https://res.cloudinary.com/djjaxwfvu/image/upload/v1672098046/CampCritic/timberline-glamping-lake-lanier_k1utvv.jpg',
+        filename: 'v1672098046/CampCritic/timberline-glamping-lake-lanier_k1utvv.jpg'
+      },
+]
