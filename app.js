86d4cd504ef314389581fb5608682ab8ed14e863
Garrett Volpe <109316247+garrettvolpe@users.noreply.github.com>
2022-12-25 22:42:44 -0500

    Update app.js

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ const mongoDBStore = require('connect-mongo');
 
 
 
-mongoose.connect("mongodb+srv://garrett:WbPC3uL0rNRm0ltC@campgrounds.pcpwmwg.mongodb.net/?retryWrites=true&w=majority", {
+mongoose.connect('mongodb+srv://garrett:WbPC3uL0rNRm0ltC@campgrounds.pcpwmwg.mongodb.net/campgrounds?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
